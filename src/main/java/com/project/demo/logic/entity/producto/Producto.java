c05a6833521aae288746b68f08c3aeda6d4f0749
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2025-06-06 14:42:39 -0600

    Add pasos del 1 al 13

---
+++ src/main/java/com/project/demo/logic/entity/producto/Producto.java
@@ -0,0 +1,71 @@
+package com.project.demo.logic.entity.producto;
+
+import com.project.demo.logic.entity.categoria.Categoria;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private int cantidadStock;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "categoria_id", referencedColumnName = "id",nullable = false)
+    private Categoria categoria;
+
+    public Producto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(int cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
---
+++ src/main/java/com/project/demo/logic/entity/user/UserSeeder.java
@@ -0,0 +1,61 @@
+package com.project.demo.logic.entity.user;
+
+import com.project.demo.logic.entity.rol.Role;
+import com.project.demo.logic.entity.rol.RoleEnum;
+import com.project.demo.logic.entity.rol.RoleRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@Order(2)
+public class UserSeeder implements ApplicationListener<ContextRefreshedEvent> {
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    public UserSeeder(
+            RoleRepository roleRepository,
+            UserRepository  userRepository,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        this.createSuperAdministrator();
+    }
+
+    private void createSuperAdministrator() {
+        User newUser = new User();
+        newUser.setName("Fabian");
+        newUser.setLastname("Miranda");
+        newUser.setEmail("fabian.miranda@gmail.com");
+        newUser.setPassword("fabianm123");
+
+        Optional<Role> optionalRole = roleRepository.findByName(RoleEnum.USER);
+        Optional<User> optionalUser = userRepository.findByEmail(newUser.getEmail());
+
+        if (optionalRole.isEmpty() || optionalUser.isPresent()) {
+            return;
+        }
+
+        var user = new User();
+        user.setName(newUser.getName());
+        user.setLastname(newUser.getLastname());
+        user.setEmail(newUser.getEmail());
+        user.setPassword(passwordEncoder.encode(newUser.getPassword()));
+        user.setRole(optionalRole.get());
+
+        userRepository.save(user);
+    }
+}
