63aee964a14a887d9bdad1018365d5f46af7d2af
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2025-06-09 10:54:13 -0600

    Add pasos 14 y 15

--- src/main/java/com/project/demo/logic/entity/categoria/CategoriaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.demo.logic.entity.categoria;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
--- src/main/java/com/project/demo/rest/categoria/CategoriaRestController.java
+++
@@ -1,4 +0,0 @@
-package com.project.demo.rest.categoria;
-
-public class CategoriaRestController {
-}
